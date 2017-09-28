8f203dbc8cea3a35a63e6e8c94c4e6c457c033f1
garamreka <garam.reka@gmail.com>
2017-09-28 12:34:44 +0200

    add todolist.txt and complete List task

--- ToDoApp/ToDoApp/ToDoApp/Program.cs
+++ ToDoApp/ToDoApp/ToDoApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,34 @@ namespace ToDoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nCommand Line Todo application"
+            if (args.Length == 0)
+            {
+                Console.WriteLine("\nCommand Line Todo application"
                 + "\n=============================\n"
                 + "\nCommand line arguments:"
                 + "\n-l   Lists all the tasks"
                 + "\n-a   Adds a new task"
                 + "\n-r   Removes a task"
                 + "\n-c   Completes a task");
+            }
+            if (args.Contains("-l"))
+            {
+                //string path = @"./todolist.txt";
 
+                try
+                {
+                    string[] content = File.ReadAllLines("Todolist.txt");
+
+                    for (int i = 0; i < content.Length; i++)
+                    {
+                        Console.WriteLine((i + 1) + " - " + content[i]);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Unable to read file.");
+                }
+            }
             Console.ReadLine();
         }
     }
